c43d5b427557f40d98ce98522de5d14f6a4eba03
Sumit Khandelwal <synergy2411@outlook.com>
2024-10-21 13:21:19 +0530

    yoga server started

--- 01-graphql-basics/src/server.js
+++ 01-graphql-basics/src/server.js
@@ -1,1 +1,29 @@
-console.log("Hello GraphQL Again");
+import { createServer } from "node:http";
+import { createSchema, createYoga } from "graphql-yoga";
+
+const PORT = 4040;
+
+const typeDefs = `
+    type Query {
+        hello: String
+    }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "World!",
+  },
+};
+
+const schema = createSchema({
+  typeDefs,
+  resolvers,
+});
+
+const yoga = createYoga({
+  schema,
+});
+
+const server = createServer(yoga);
+
+server.listen(PORT, () => console.log(`Yoga Server started at PORT : ${PORT}`));
