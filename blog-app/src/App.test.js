f0c7f5ded7df0a39b38fa5610af724f858bef2fe
Sumit Khandelwal <synergy2411@outlook.com>
2024-10-23 15:43:51 +0530

    test added

--- blog-app/src/App.test.js
+++ blog-app/src/App.test.js
@@ -1,8 +1,61 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { ApolloClient, InMemoryCache, HttpLink, gql } from "@apollo/client";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("GraphQL Server API Testing", () => {
+  let client = null;
+
+  beforeEach(() => {
+    client = new ApolloClient({
+      uri: "http://localhost:4000/graphql",
+      cache: new InMemoryCache(),
+    });
+  });
+
+  afterEach(() => {
+    client = null;
+  });
+
+  test("should fetch all posts from Graphql Server", async () => {
+    const FETCH_POSTS = gql`
+      query fetchPosts {
+        posts {
+          id
+          title
+          body
+          published
+        }
+      }
+    `;
+
+    const { data } = await client.query({
+      query: FETCH_POSTS,
+    });
+
+    expect(data).not.toBeUndefined();
+    expect(data.posts.length).not.toEqual(0);
+  });
+
+  test("should provide token on valid credentials", async () => {
+    const USER_SIGN_IN = gql`
+      mutation signIn($email: String!, $password: String!) {
+        signIn(data: { email: $email, password: $password }) {
+          token
+        }
+      }
+    `;
+
+    const { data } = await client.mutate({
+      mutation: USER_SIGN_IN,
+      variables: {
+        email: "ross@test",
+        password: "ross123",
+      },
+    });
+
+    expect(data).not.toBeUndefined();
+    expect(data.signIn.token).not.toBeUndefined();
+  });
+
+  test("should pass the test", () => {
+    expect(true).toBeTruthy();
+  });
 });
